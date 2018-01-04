7548980a9c783c0bfc3f39fb249cba724082554f
ulaganathank <ulaganathank@ebraintechnologies.com>
2018-01-04 12:46:30 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import { FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'home',component:HomeComponent},
+      {path:'forms',component:FormsComponent},
+      {path:'',redirectTo:'/home',pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
